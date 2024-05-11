b4263891dac841d67ec4e83909543dab680774ff
Cristian Bernal <cr.bernalo@duocuc.cl>
2024-05-10 21:50:47 -0400

    creacion del proyecto de prueba js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    const Swal = require('sweetalert2')
+
+    AOS.init();
+    $('#summernote').summernote();
+    let table = new DataTable('#tablita');
+        
+});
